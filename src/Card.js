0c280e87bd876fc88c76b8c53f333b8fc1647ad7
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2022-08-17 15:07:39 +0530

    phone arguement with component and css timing

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const   Card = (props) => {
-    const {id,name,email} = props; 
+    const {id,name,email,phone} = props; 
     return(
         <div className='container one'>            
             <div className='card'>
                 <img alt = 'one' src = {`https://robohash.org/${id}?size=250x250`}/>
                 <h2>{name}</h2>
                 <p>{email}</p>
+                <p>Phone number is {phone}</p>
             </div>
         </div>
     )
