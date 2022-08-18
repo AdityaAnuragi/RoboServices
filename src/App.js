f93e9e53b1e76812190e2634e32ae915dfd374aa
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2022-08-18 23:10:28 +0530

    removed a space

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class AppComp extends Component {
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json()) 
+            .then(response => response.json())
             .then(users => this.setState({robots : users}));
         
     }
